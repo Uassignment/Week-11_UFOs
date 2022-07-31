8547b57288b01d593f473608f786b1c2faaa56ea
Uassignment <102333060+Uassignment@users.noreply.github.com>
2022-07-31 13:35:03 -0500

    filter function debugged

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,8 @@
 const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+
 function buildTable(data) {
   // clear existing data
   tbody.html("");
@@ -19,39 +21,56 @@ function buildTable(data) {
     );
   });
 }
-function filteredTable() {
-  
-    var date, city, state, country, shape, filteredData;
 
-    date = d3.select("#datetime").property("value");
-    city = d3.select("#city").property("value");
-    state = d3.select("#state").property("value");
-    country = d3.select("#country").property("value");
-    shape = d3.select("#shape").property("value");
-    filteredData = tableData;
-    
-  if (date){
+function filteredTable() {
+
+  var date, city, state, country, shape, filteredData;
+
+  date = d3.select("#datetime").property("value");
+  city = d3.select("#city").property("value");
+  state = d3.select("#state").property("value");
+  country = d3.select("#country").property("value");
+  shape = d3.select("#shape").property("value");
+  filteredData = tableData;
+
+  if (date) {
     filteredData = filteredData.filter(row => row.datetime === date);
   }
-  if (city){
+  if (city) {
     filteredData = filteredData.filter(row => row.city === city);
   }
-  if (state){
+  if (state) {
     filteredData = filteredData.filter(row => row.state === state);
   }
-  if (country){
+  if (country) {
     filteredData = filteredData.filter(row => row.country === country);
   }
-  if (shape){
+  if (shape) {
     filteredData = filteredData.filter(row => row.shape === shape);
   }
 
-// Rebuild the table using the filtered data
+  
+  //buildTable(clearFilter);
 
-// just be the original tableData.
-buildTable(filteredData);
+  buildTable(filteredData);
+  
 }
+
+buildTable(tableData);
+
 d3.selectAll("#filter-btn").on("click", filteredTable);
+d3.selectAll("#clearFilter").on("click", clearFilter);
+
+
 
 // Build the table when the page loads
-buildTable(tableData);
+
+
+//function showAll() {
+  //var $rows = $('#findings tr');
+  //$rows.show();
+//}
+//var buttonClearFilter = $('#clearFilter');
+//buttonClearFilter.click('showaAll');
+
+
